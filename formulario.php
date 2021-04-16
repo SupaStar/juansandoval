bac572c440960ec102f09634b7800d19cefab7c2
Obed Noe Martinez Gonzalez <68682799+SupaStar@users.noreply.github.com>
2021-04-16 18:42:53 -0500

    Correos por smtp

--- formulario.php
+++ formulario.php
@@ -1,14 +1,37 @@
 <?php
-$remitente = $_POST['correo'];
-$destinatario = "alfredomendez60@gmail.com";
-$asunto = "Te contactaron en el formulario de tu sitio web";
-$fijo = ' ';
-if (isset($_POST['nfijo'])){
-    $fijo = ' Mi numero de telefono fijo es: ' . $_POST['nfijo'] . ' ';
+use PHPMailer\PHPMailer\PHPMailer;
+require ("src/PHPMailer.php");
+require ("src/SMTP.php");
+require ("src/Exception.php");
+$mail = new PHPMailer(true);
+try {
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'pruebasmodulo5cetis@gmail.com';
+    $mail->Password = 'Ulisestortuga1';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+    $mail->setFrom($_POST['correo'], $_POST['nombre']);
+    $mail->addAddress('alfredomendez60@gmail.com', 'Alfredo');
+    $mail->isHTML(true);
+    $mail->Subject = 'Te contactaron en el formulario de tu sitio web';
+    $fijo = ' ';
+    if (isset($_POST['nfijo '])){
+        $fijo = ' Mi numero de telefono fijo es: ' . $_POST['nfijo '] . ' ';
+    }
+    $telefono = 'Mi telefono de celular es: ' . $_POST['ncelular'] . ' te contacte con el siguiente asunto:';
+    $mensaje = "Mi nombre es: " . $_POST['nombre'] . $fijo . $telefono .'<div>'. $_POST['asunto'].'</div>';
+    $mail->Body = $mensaje;
+    $mail->send();
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-$telefono = 'Mi telefono de celular es: ' . $_POST['ncelular'] . ' te contacte con el siguiente asunto:\n';
-$mensaje = "Mi nombre es: " . $_POST['nombre'] . $fijo . $telefono . $_POST['asunto'];
-$headers = "From: $remitente\r\nReply-to: $remitente";
-
-mail($destinatario, $asunto, $mensaje, $headers);
 ?>
